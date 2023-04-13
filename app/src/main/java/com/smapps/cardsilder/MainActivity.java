b8796f90f4bf487684d7de3bd05a3945ba924aa1
Fahamin <fahamin00@gmail.com>
2023-04-13 15:51:36 +0600

    Initial commit

---
+++ app/src/main/java/com/smapps/cardsilder/MainActivity.java
@@ -0,0 +1,49 @@
+package com.smapps.cardsilder;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+
+import com.smarteist.autoimageslider.IndicatorView.animation.type.IndicatorAnimationType;
+import com.smarteist.autoimageslider.SliderAnimations;
+import com.smarteist.autoimageslider.SliderView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    SliderView sliderView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        sliderView = findViewById(R.id.imageSlider);
+
+
+        setSliderViews();
+    }
+
+    private void setSliderViews() {
+
+        List<SliderItem> list = new ArrayList<>();
+        list.add(new SliderItem("ONE",""));
+        list.add(new SliderItem("TWO",""));
+        list.add(new SliderItem("THREE",""));
+        list.add(new SliderItem("FOUR",""));
+
+
+        SliderAdapterExample adapter = new SliderAdapterExample(this,list);
+        sliderView.setSliderAdapter(adapter);
+        sliderView.setIndicatorAnimation(IndicatorAnimationType.WORM); //set indicator animation by using IndicatorAnimationType. :WORM or THIN_WORM or COLOR or DROP or FILL or NONE or SCALE or SCALE_DOWN or SLIDE and SWAP!!
+        sliderView.setSliderTransformAnimation(SliderAnimations.SIMPLETRANSFORMATION);
+        sliderView.setAutoCycleDirection(SliderView.AUTO_CYCLE_DIRECTION_BACK_AND_FORTH);
+        sliderView.setIndicatorSelectedColor(Color.WHITE);
+        sliderView.setIndicatorUnselectedColor(Color.GRAY);
+        sliderView.setScrollTimeInSec(4); //set scroll delay in seconds :
+        sliderView.startAutoCycle();
+    }
+}
